3b3f8e47a1acf030853b0947be1d930f32f6aca8
joeltancej <joeltancej@gmail.com>
2023-03-01 04:44:26 +0800

    PSI Manager to get data from PSI API

--- APIManagers/psimanager.py
+++ APIManagers/psimanager.py
@@ -1,29 +1,21 @@
 # import requests library to make requests and work with API
 import requests
-# import pytz to get SGT
-import pytz
-import datetime
+# import from sgdatetime to get the SGT parameters
+import sgdatetime
 
 # the "region" parameter should be of type string and contain one of the following 6 values:
 # "west", "national", "east", "central", "south", "north"
 
 def getpsi(region):
-    # set timezone to SG
-    timezone = pytz.timezone('Asia/Singapore')
-
-    # get datetime and localize it to SGT
-    dt = datetime.datetime.now()
-    now = timezone.localize(dt)
-
-    # transform datetime into a format suitable for the PSI API
-    param = now.strftime("%Y-%m-%d %H:%M:%S").replace(" ", "T")
-
     # making a GET request to the API endpoint, obtain output in a dictionary format with .json()
-    response = requests.get("https://api.data.gov.sg/v1/environment/psi", param).json()
+    response = requests.get("https://api.data.gov.sg/v1/environment/psi", sgdatetime.get_dt()).json()
     psi = response["items"][0]["readings"]["psi_twenty_four_hourly"][region]
 
     # print(type(response.items))
     return psi
+
+
+
 
 # for testing reasons:
 # def main():
