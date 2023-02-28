d9ab543a4779c8992685001649df1d729a4a948c
joeltancej <joeltancej@gmail.com>
2023-03-01 04:45:28 +0800

    To get SGT datetime

---
+++ APIManagers/sgdatetime.py
@@ -0,0 +1,18 @@
+# for the API Managers to get SGT datetime
+
+# import pytz to get SGT
+import pytz
+import datetime
+
+def get_dt():
+    # set timezone to SG
+    timezone = pytz.timezone('Asia/Singapore')
+
+    # get datetime and localize it to SGT
+    dt = datetime.datetime.now()
+    now = timezone.localize(dt)
+
+    # transform datetime into a format suitable for the PSI API
+    param = now.strftime("%Y-%m-%d %H:%M:%S").replace(" ", "T")
+
+    return param
