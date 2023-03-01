d1d8fcfff94580ee13f4499a1e4b82248f0f022b
MangoTMH <80561272+MangoTMH@users.noreply.github.com>
2023-03-01 23:43:34 +0800

    Create database.py

---
+++ database.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine, text
+
+db_connection_string = "mysql+pymysql://2naahj3wjte7sag4lgll:pscale_pw_aHoiZ6aD6S7rZCyYDAlUAXxjie1DrD5oxLxXDTaBCqh@ap-southeast.connect.psdb.cloud/sportsgowhere_db?charset=utf8mb4"
+
+engine = create_engine(
+    db_connection_string,
+    connect_args={
+        "ssl": {
+            "ssl_ca": "/etc/ssl/cert.pem"
+        }
+    })
+
+
+def load_accounts_from_db():
+    with engine.connect() as conn:
+        result = conn.execute(text("select * from Accounts"))
+        accounts = []
+        for row in result.all():
+            accounts.append(dict(row))
+        return accounts
