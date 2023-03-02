0884e8b453497d148fc4dfb4c978602c1cee46d2
kenfyxx <kenfongyexuan@gmail.com>
2023-03-02 16:08:59 +0800

    update1

--- Flask_Learn/flaskblog/models.py
+++ Flask_Learn/flaskblog/models.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from itsdangerous import URLSafeTimedSerializer as Serializer
 from flaskblog import db, login_manager, app
 from flask_login import UserMixin
 
